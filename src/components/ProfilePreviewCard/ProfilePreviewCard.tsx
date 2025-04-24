1ff5445c01202a5bfe74c43c53b4a8edb0482d71
KiBohr <katharinabohr@web.de>
2025-04-24 12:06:01 +0200

    ppp link added

--- src/components/ProfilePreviewCard/ProfilePreviewCard.tsx
+++ src/components/ProfilePreviewCard/ProfilePreviewCard.tsx
@@ -1,11 +1,13 @@
+import { Link } from "react-router-dom";
 import MainButton from "../MainButton/MainButton";
 
 const ProfilePreviewCard = () => {
-    // bild und name sind klickbar zu profil
+    // bild und name sind klickbar zu profil ✔︎
     return (  
         <article className="px-10 w-full flex justify-between items-center">
+            <Link to="/profile">
             <div className="flex items-center justify-center gap-5">
-                <div className="h-15 w-15">
+                <div className="h-10 w-10">
                     <img className="h-full object-fill" src="/public/svg/ProfilePlaceholder.png" alt="profilepicture placeholder" />
                 </div>
                 <div className="flex flex-col ">
@@ -13,8 +15,10 @@ const ProfilePreviewCard = () => {
                     <p className=" text-[0.8rem] font-extralight opacity-70">proffession</p>
                 </div>
             </div>
+            </Link>
             
-        <p>button</p>
+            {/* nur solange der mainbutton noch keine funktion hat, dann gerne löschen */}
+        <p>button</p> 
             <MainButton/>
         </article>
         
