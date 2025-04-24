08a367c071628ac935b1da4fe4d68175f50fd019
KiBohr <katharinabohr@web.de>
2025-04-24 12:05:23 +0200

    ProfilePreviewCard done

--- src/components/ProfilePreviewCard/ProfilePreviewCard.tsx
+++ src/components/ProfilePreviewCard/ProfilePreviewCard.tsx
@@ -3,10 +3,24 @@ import MainButton from "../MainButton/MainButton";
 const ProfilePreviewCard = () => {
     // bild und name sind klickbar zu profil
     return (  
-        <>
-            <h1>ProfilePreviewCard</h1>
+        <article className="px-10 w-full flex justify-between items-center">
+            <div className="flex items-center justify-center gap-5">
+                <div className="h-15 w-15">
+                    <img className="h-full object-fill" src="/public/svg/ProfilePlaceholder.png" alt="profilepicture placeholder" />
+                </div>
+                <div className="flex flex-col ">
+                    <p className="font-bold">profile_name</p>
+                    <p className=" text-[0.8rem] font-extralight opacity-70">proffession</p>
+                </div>
+            </div>
+            
+        <p>button</p>
             <MainButton/>
-        </>
+        </article>
+        
+            
+            
+        
     );
 }
 
