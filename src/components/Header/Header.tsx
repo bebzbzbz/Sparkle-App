7ea44b05237c8c13f8168aef1c2fde61f0c5ea84
bebzbzbz <balzerbeatrice@gmail.com>
2025-04-24 15:26:09 +0200

    header expand

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,21 +1,29 @@
+import { useNavigate } from "react-router-dom";
+
 interface HeaderProps {
     headerTitle: string,
     imgLeft: string,
-    leftAction?: () => {},
-    imgRight?: string,
-    rightAction?: () => {}
+    leftLinkDestination?: () => {},
+    imgRight1?: string,
+    rightAction1?: () => {}
+    imgRight2?: string,
+    rightAction2?: () => {}
+    imgRight3?: string,
+    rightAction3?: () => {}
 }
 
-const Header = ({headerTitle, imgLeft, leftAction, imgRight, rightAction} : HeaderProps) => {
+const Header = ({headerTitle, imgLeft, leftLinkDestination, imgRight1, imgRight2, imgRight3, rightAction1, rightAction2, rightAction3} : HeaderProps) => {
+    const navigate = useNavigate();
+
     return (  
         <header 
             className="flex justify-between px-20">
             <h1
-                className={`flex items-center gap-3 ${rightAction && "cursor-pointer"}`}
+                className={`flex items-center gap-3 ${leftLinkDestination && "cursor-pointer"}`}
                 onClick={() => {
                     // wenn eine aktion übergeben wurde, wird diese auslöst, ansonsten geschieht nichts
-                    if(!!leftAction) {
-                        leftAction
+                    if(!!leftLinkDestination) {
+                        navigate(`/${leftLinkDestination}`)
                     }
                 }}>
                 <img 
@@ -24,17 +32,39 @@ const Header = ({headerTitle, imgLeft, leftAction, imgRight, rightAction} : Head
                     className="h-8"/>
                 {headerTitle}
             </h1>
-            {imgRight && 
-            <img 
-            src={`/svg/${imgRight}.svg`} 
-            alt="Likes" 
-            className={`${leftAction && "cursor-pointer"}`}
-            onClick={() => {
-                if(!!rightAction) {
-                    rightAction
-                }
-            }}/>}
-            
+            <div
+                className="flex gap-5">
+                {imgRight1 && 
+                <img 
+                src={`/svg/${imgRight1}.svg`} 
+                alt="Likes" 
+                className={`${rightAction1 && "cursor-pointer"}`}
+                onClick={() => {
+                    if(!!rightAction1) {
+                        rightAction1
+                    }
+                }}/>}
+                {imgRight2 && 
+                <img 
+                src={`/svg/${imgRight2}.svg`} 
+                alt="Likes" 
+                className={`${rightAction2 && "cursor-pointer"}`}
+                onClick={() => {
+                    if(!!rightAction2) {
+                        rightAction2
+                    }
+                }}/>}
+                {imgRight3 && 
+                <img 
+                src={`/svg/${imgRight3}.svg`} 
+                alt="Likes" 
+                className={`${rightAction3 && "cursor-pointer"}`}
+                onClick={() => {
+                    if(!!rightAction3) {
+                        rightAction3
+                    }
+                }}/>}
+            </div>
         </header>
     );
 }
