c03e2b52039fd6157d11161ff3fe947fd7db0a7c
bebzbzbz <balzerbeatrice@gmail.com>
2025-04-29 14:55:23 +0200

    comments can be deleted

--- src/components/CommentsModal/CommentsModal.tsx
+++ src/components/CommentsModal/CommentsModal.tsx
@@ -67,7 +67,7 @@ const CommentsModal = ({allComments, setShowCommentModal, handleCommentSubmit, c
               <p className="text-gray-500">No comments yet. Be the first!</p>
             )}
             {allComments.map((c) => (
-              <div key={c.id} className="flex gap-2">
+              <div key={crypto.randomUUID()} className="flex gap-2">
                 <img src={c.profile_image_url} alt="" className="object-cover rounded-full h-8 w-8" />
 
                 <div className="w-full">
