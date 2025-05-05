b5510ecc9b19a5c012f7745ea0c803825b2beaad
[PeterWolf242] <[peter.ebm@web.de]>
2025-05-05 09:17:24 +0200

    intro-page

--- src/App.tsx
+++ src/App.tsx
@@ -12,34 +12,36 @@ import Favorites from "./pages/Favorites/Favorites"
 import NewPost from "./pages/NewPost/NewPost"
 import Explore from "./pages/Explore/Explore"
 import AboutSparkle from "./pages/AboutSparkle/AboutSparkle"
+import IntroPage from "./pages/IntroPage/IntroPage"
 
 function App() {
-  const router = createBrowserRouter(createRoutesFromElements(
-    <>
-      <Route path="/signup" element={<SignUp/>}/>
-      <Route path="/login" element={<Login/>}/>
-      <Route element={<ProtectedRoute />}>
-        <Route path="/" element={<Layout/>}>
-          <Route index element={<Navigate to="/home" replace />} />
-          <Route path="home" element={<Home/>}/>
-          <Route path="explore" element={<Explore/>}/>
-          <Route path="profile" element={<UserProfile/>}/>
-          <Route path="profile/edit" element={<EditProfile/>}/>
-          <Route path="users/:userParam" element={<CommunityProfile/>}/>
-          <Route path="search" element={<SearchAll/>}/>
-          <Route path="newpost" element={<NewPost/>}/>
-          <Route path="favorites" element={<Favorites/>}/>
-          <Route path="about-sparkle" element={<AboutSparkle/>}/>
-        </Route>
-      </Route>
-    </>
-  ))
+	const router = createBrowserRouter(createRoutesFromElements(
+		<>
+			<Route path="/signup" element={<SignUp />} />
+			<Route path="/login" element={<Login />} />
+			<Route path="intropage" element={<IntroPage />} />
+			<Route element={<ProtectedRoute />}>
+				<Route path="/" element={<Layout />}>
+					<Route index element={<Navigate to="/IntroPage" replace />} />
+					<Route path="home" element={<Home />} />
+					<Route path="explore" element={<Explore />} />
+					<Route path="profile" element={<UserProfile />} />
+					<Route path="profile/edit" element={<EditProfile />} />
+					<Route path="users/:userParam" element={<CommunityProfile />} />
+					<Route path="search" element={<SearchAll />} />
+					<Route path="newpost" element={<NewPost />} />
+					<Route path="favorites" element={<Favorites />} />
+					<Route path="about-sparkle" element={<AboutSparkle />} />
+				</Route>
+			</Route>
+		</>
+	))
 
-  return (
-    <>
-      <RouterProvider router={router}/>
-    </>
-  )
+	return (
+		<>
+			<RouterProvider router={router} />
+		</>
+	)
 }
 
 export default App
---
+++ src/components/intro/Intro.tsx
@@ -0,0 +1,29 @@
+import "./Intro.css"
+import { useEffect } from "react"
+import { useNavigate } from "react-router-dom"
+
+export default function Intro() {
+	const navigate = useNavigate()
+
+	useEffect(() => {
+		const timer = setTimeout(() => {
+			navigate("/login")
+		}, 8000)
+
+		return () => clearTimeout(timer)
+	}, [navigate])
+
+	return (
+		<div className="intro-container mt-0">
+			<div className="sparkle-wrapper">
+				<img className="star-medium" src="/svg/star-medium.svg" alt="Star-Medium" />
+
+				<img className="sparkle-text" src="/svg/sparkle-text.svg" alt="Sparkle-Logo" />
+
+				<img className="star-large" src="/svg/star-medium.svg" alt="Star-large" />
+
+				<img className="star-small" src="/svg/star-medium.svg" alt="Star-small" />
+			</div>
+		</div>
+	)
+}
